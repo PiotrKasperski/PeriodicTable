acac25029ef8f76220aa104c2247f91074baa6aa
Piotr Kasperski <piotr.kasperski94@gmail.com>
2024-09-09 22:37:12 +0200

    added filtration to table (#8)

--- src/app/periodic-table/periodic-table.component.ts
+++ src/app/periodic-table/periodic-table.component.ts
@@ -8,27 +8,43 @@ import {
     WritableSignal,
 } from '@angular/core'
 import { PeriodicElement } from '../interfaces/periodic-element'
-import { MatTableModule } from '@angular/material/table'
+import { MatTableDataSource, MatTableModule } from '@angular/material/table'
 import { MatDialog } from '@angular/material/dialog'
 import { PeriodicTableEditDialogComponent } from '../periodic-table-edit-dialog/periodic-table-edit-dialog.component'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms'
+import { debounceTime, filter } from 'rxjs'
 
 @Component({
     selector: 'app-periodic-table',
     standalone: true,
-    imports: [MatTableModule],
+    imports: [
+        MatTableModule,
+        MatFormFieldModule,
+        MatInputModule,
+        ReactiveFormsModule,
+    ],
     templateUrl: './periodic-table.component.html',
     styleUrl: './periodic-table.component.scss',
 })
 export class PeriodicTableComponent implements OnInit {
+    dialog = inject(MatDialog)
+    fb = inject(FormBuilder)
     data = input.required<PeriodicElement[]>()
     periodicElements: WritableSignal<PeriodicElement[]> = signal([])
-    displayedElements = computed(() =>
-        this.periodicElements().filter(element => true)
+    displayedElements = computed(
+        () => new MatTableDataSource(this.periodicElements())
     )
     displayedColumns: string[] = ['position', 'name', 'weight', 'symbol']
-    dialog = inject(MatDialog)
+    filter = this.fb.group({ value: '' })
     ngOnInit(): void {
         this.periodicElements.set(this.data())
+        this.filter.valueChanges
+            .pipe(debounceTime(2000))
+            .subscribe(
+                filter => (this.displayedElements().filter = filter.value ?? '')
+            )
     }
     onRowClicked(element: PeriodicElement): void {
         const dialogRef = this.dialog.open(PeriodicTableEditDialogComponent, {
